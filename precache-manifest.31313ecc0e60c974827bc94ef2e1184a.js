c7b9a2f82d2a6381bd3dcad298cc317b29246fb8
Aneeta Binoy <aneetab@gmail.com>
2020-05-25 10:04:21 +0530

    Updates

---
+++ precache-manifest.31313ecc0e60c974827bc94ef2e1184a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c61375847cebfff46c0e0a60aac4bf",
+    "url": "/React-in-git-demo/index.html"
+  },
+  {
+    "revision": "f3c6a3523c98db7a10e2",
+    "url": "/React-in-git-demo/static/css/main.e02355d7.chunk.css"
+  },
+  {
+    "revision": "09c9b17dc9a48d9cb41f",
+    "url": "/React-in-git-demo/static/js/2.7862c346.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-in-git-demo/static/js/2.7862c346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c6a3523c98db7a10e2",
+    "url": "/React-in-git-demo/static/js/main.694766db.chunk.js"
+  },
+  {
+    "revision": "37a2bccb8e69f5e1f051",
+    "url": "/React-in-git-demo/static/js/runtime-main.2ee47839.js"
+  }
+]);
